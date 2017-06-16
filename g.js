9b88ff0ef6459ecad73c383ec578a449b2f09650
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-16 11:54:57 +0700

    het

---
+++ g.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Pho',
+    age: 18
+};
+
+function a(x, y) {
+    console.log(x + y);
+}
+
+module.exports = {
+    fa: a,
+    op: person
+};
---
+++ h.js
@@ -0,0 +1,5 @@
+const f = require('./g');
+
+f.fa(4, 5);
+
+console.log(f.op);
