a10b3feabd130939b7739ce943975f6eabdff7d0
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-16 08:48:30 +0700

    setup

---
+++ a.js
@@ -0,0 +1,14 @@
+// name, age, height
+class Person {
+    constructor(name, age, height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+}
+
+const teo = new Person('Teo', 18, 150);
+const ti = new Pesron('Ti', 10, 180);
+const tun = new Person('Tun', 20, 140);
+
+const arrPerson = [teo, ti, tun];
