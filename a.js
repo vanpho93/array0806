21104bcbc0e5cdcce4058ae78e5888fc9f3fbc31
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-16 09:17:09 +0700

    check

--- a.js
+++ a.js
@@ -8,7 +8,20 @@ class Person {
 }
 
 const teo = new Person('Teo', 18, 150);
-const ti = new Pesron('Ti', 10, 180);
+const ti = new Person('Ti', 10, 180);
 const tun = new Person('Tun', 20, 140);
 
 const arrPerson = [teo, ti, tun];
+
+// arrPerson.forEach((e, i) => console.log(i + 1 + ': ' + e.name));
+
+function check(arr, checkFunction) {
+    for (let i = 0; i < arr.length; i++) {
+        if (checkFunction(arr[i])) return true;
+    }
+    return false;
+}
+
+console.log(check(arrPerson, e => e.height > 190));
+
+console.log(check(arrPerson, e => e.age > 18));
